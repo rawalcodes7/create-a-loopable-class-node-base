fbeff78c5645ea3c73ffaf9f4ce5e99d492acd62
nirmal.rawal <rawal.nirmal7@gmail.com>
2023-06-02 17:46:17 +0530

    assignment

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,4 +1,37 @@
+import java.util.Iterator;
 // write your code here
-public class Node {
+public class Node implements Iterable<Node> {
+    public Node next;
+    public int data;
+    public Node(Node next, int data){
+        this.next=next;
+        this.data=data;
+    }
+    public Node(int data){
+        this.data=data;
+    }
 
+    @Override
+    public Iterator<Node> iterator() {
+        return new NodeIterator(this);
+    }
+    public static class NodeIterator implements Iterator<Node>{
+        private Node head;
+
+        public NodeIterator(Node head){
+            this.head=head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.head!=null;
+        }
+
+        @Override
+        public Node next() {
+            Node temp=head;
+            head=head.next;
+            return temp;
+        }
+    }
 }
